c64649d2952ab7a8ac0d1f880faf7a346a74734c
Nicholas Ng <nicholasnkk@gmail.com>
2024-07-15 16:07:40 +0800

    added overrides

--- app/include/engine/kedro.hpp
+++ app/include/engine/kedro.hpp
@@ -6,4 +6,7 @@ class Kedro : public AbstractEngine
 {
 public:
     Kedro();
+    virtual bool execute(QtNodes::DataFlowGraphModel *model) override;
+    virtual bool validityCheck(QtNodes::DataFlowGraphModel *model) override;
+    virtual QVariant getNodeOutput(QtNodes::NodeId id) override;
 };
--- app/src/engine/kedro.cpp
+++ app/src/engine/kedro.cpp
@@ -7,3 +7,18 @@ using QtNodes::DataFlowGraphModel;
 Kedro::Kedro()
 {
 }
+
+bool Kedro::execute(QtNodes::DataFlowGraphModel *model)
+{
+    return false;
+}
+
+bool Kedro::validityCheck(QtNodes::DataFlowGraphModel *model)
+{
+    return false;
+}
+
+QVariant Kedro::getNodeOutput(QtNodes::NodeId id)
+{
+    return QVariant();
+}
